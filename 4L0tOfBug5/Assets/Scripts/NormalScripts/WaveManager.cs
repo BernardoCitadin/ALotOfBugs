42a5b40504093191c7fbf4d3b8c25500b4cc17e4
BernardoCitadin <bernardocitadin@gmail.com>
2024-05-25 14:46:19 -0300

    WaveSystem (made, not implemented)

---
+++ 4L0tOfBug5/Assets/Scripts/NormalScripts/WaveManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timeText, waveText;
+    public static WaveManager Instance;
+
+    bool waveRunning = true;
+    int currentWave = 0;
+    int currentWaveTime;
+
+    private void Awake()
+    {
+        if(Instance == null) Instance = this;
+    }
+    private void Start()
+    {
+        StartNewWave();
+        timeText.text = "30";
+        waveText.text = "Wave: 1";
+    }
+    void StartNewWave()
+    {
+        StopAllCoroutines();
+        timeText.color = Color.white;
+        currentWave++;
+        waveRunning = true;
+        currentWaveTime = 30;
+        waveText.text = "Wave: " + currentWave;
+        StartCoroutine(WaveTimer());
+    }
+
+    IEnumerator WaveTimer()
+    {
+        while(waveRunning)
+        {
+            yield return new WaitForSeconds(1f);
+            currentWaveTime--;
+
+            timeText.text = currentWaveTime.ToString();
+
+            if (currentWaveTime <= 0)
+                WaveComplete();
+        }
+        yield return null;
+    }
+    void WaveComplete()
+    {
+        StopAllCoroutines();
+        waveRunning = false;
+        EnemyManager.Instance.DestroyAllEnemies();
+        currentWaveTime = 30;
+        timeText.text = currentWaveTime.ToString();
+        timeText.color = Color.red;
+    }
+}
