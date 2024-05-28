dfe3e2ece0a3cb9451dc3d0a8f2a6e300c412d38
mesiiue <bernardocitadin@gmail.com>
2024-05-28 14:39:52 -0300

    StartedSword

---
+++ 4L0tOfBug5/Assets/Scripts/NormalScripts/Guns/MeleeWeapon.cs
@@ -0,0 +1,117 @@
+using UnityEngine;
+
+public class MeleeWeapon : MonoBehaviour
+{
+    [Header("Prefabs")]
+    [SerializeField] GameObject muzzle;
+    [SerializeField] Transform muzzlePosition;
+    [SerializeField] GameObject projectile;
+    [SerializeField] Transform player;
+    [SerializeField] Transform bullets;
+
+    [Space(20)]
+
+    [Header("Config")]
+    [SerializeField] public float fireDistance = 10f;
+    [SerializeField] public float fireRate = 0.5f;
+
+
+    Vector2 offset;
+
+
+    private float lastShotTime = 0;
+    Transform closeEnemy;
+    Animator anim;
+
+    public static MeleeWeapon Instance;
+    private void Awake()
+    {
+        Instance = this;
+    }
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        bullets = GameObject.FindGameObjectWithTag("Bullets").transform;
+        anim = GetComponent<Animator>();
+        lastShotTime = fireRate;
+        print(bullets.name);
+    }
+
+    void Update()
+    {
+        transform.position = (Vector2)player.position + offset;
+
+        FindCloseEnemy();
+        Aim();
+        Shooting();
+
+        if (player == null)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void FindCloseEnemy()
+    {
+        closeEnemy = null;
+        float closeDistance = Mathf.Infinity;
+
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+
+        foreach (Enemy enemy in enemies)
+        {
+            float distance = Vector2.Distance(transform.position, enemy.transform.position);
+            if (distance < closeDistance && distance < fireDistance)
+            {
+                closeDistance = distance;
+                closeEnemy = enemy.transform;
+            }
+        }
+    }
+
+    void Aim()
+    {
+        if (closeEnemy != null)
+        {
+            Vector3 direction = closeEnemy.position - transform.position;
+            direction.Normalize();
+
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+
+            transform.position = (Vector2)player.position + offset;
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+
+    void Shooting()
+    {
+        if (closeEnemy == null) return;
+
+        lastShotTime += Time.deltaTime;
+        if (lastShotTime >= fireRate)
+        {
+            Shoot();
+            lastShotTime = 0;
+        }
+    }
+
+    void Shoot()
+    {
+        var muzzleGo = Instantiate(muzzle, muzzlePosition.position, transform.rotation);
+        muzzleGo.transform.SetParent(transform);
+        Destroy(muzzleGo, 0.05f);
+
+        var projectileGo = Instantiate(projectile, muzzlePosition.position, transform.rotation, bullets);
+        Destroy(projectileGo, 3);
+    }
+
+    public void SetOffset(Vector2 o)
+    {
+        offset = o;
+    }
+}
