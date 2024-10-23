d4f530b27cdc2194d14bfd8df3c0ce2745e02967
mesiiue <bernardocitadin@gmail.com>
2024-10-23 10:55:44 -0300

    canvaBugAdded

--- 4L0tOfBug5/Assets/CanvaScale.cs
+++ 4L0tOfBug5/Assets/CanvaScale.cs
@@ -1,9 +1,17 @@
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class CanvaScale : MonoBehaviour
 {
-    public void ScaleCanva()
+    public UnityEvent OnCanved;
+    public void ScaleCanva(CanvasScaler canvas)
     {
-
+        canvas.uiScaleMode = (CanvasScaler.ScaleMode)0;
     }
+    public void DiscaleCanva(CanvasScaler canvas)
+    {
+        canvas.uiScaleMode = (CanvasScaler.ScaleMode)1;
+    }
+
 }
